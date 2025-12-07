6e6df363f295728a9f99cd6de7a0f7a2a80aea5f
skimransabuj <sabujx9@gmail.com>
2025-12-07 23:11:41 +0600

    new products section responsive

--- src/components/HomePage/Products/NewProducts.tsx
+++ src/components/HomePage/Products/NewProducts.tsx
@@ -4,14 +4,26 @@ import { ProductCard } from "./ProductCard";
 
 const NewProducts = async () => {
   const newProducts = await getAllProducts(10);
-  console.log({ newProducts });
+
   return (
-    <Container className="px-12 py-12">
+    <Container className="px-4 sm:px-8 md:px-12 py-12">
       <div className="px-2">
-        <h2 className="text-2xl text-brand-accent-light ml-10 mb-2">
+        <h2 className="text-xl sm:text-2xl text-brand-accent-light ml-2 sm:ml-10 mb-4">
           New <span className="text-black">Arrivals</span>
         </h2>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-x-6 gap-y-8">
+
+        <div
+          className="
+          grid 
+          grid-cols-2 
+          sm:grid-cols-3 
+          md:grid-cols-4 
+          lg:grid-cols-5 
+          xl:grid-cols-6 
+          gap-x-4 sm:gap-x-6 
+          gap-y-6 sm:gap-y-8
+        "
+        >
           {newProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
